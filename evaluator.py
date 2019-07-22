82f594c67132dc13859654a35149036c8a44257d
ranreichman <ran.reichman@snip.today>
2019-07-21 23:11:11 -0700

    got all the scoring tests working

--- evaluator.py
+++ evaluator.py
@@ -1,5 +1,19 @@
-VALUE_PRESETS = {"straight_flush" : 10000, "four_of_kind" : 9000, "full_house" : 8000, "flush" : 7000,
-                 "straight": 6000, "three_of_kind" : 5000, "two_pair" : 4000, "pair" : 3000, "high_card" : 2000}
+import math
+
+POWER_PRESET = 100000000
+VALUE_PRESETS = {"straight_flush" : 10 * POWER_PRESET, "four_of_kind" : 9 * POWER_PRESET,
+                 "full_house" : 8 * POWER_PRESET, "flush" : 7 * POWER_PRESET, "straight": 6 * POWER_PRESET,
+                 "three_of_kind" : 5 * POWER_PRESET, "two_pair" : 4 * POWER_PRESET, "pair" : 3 * POWER_PRESET}
+
+
+def getHighestCardScore(cards, number_of_cards_to_count):
+    sorted_by_rank = sorted(cards, key=lambda x: x[0])
+
+    score = 0
+    for i in range(number_of_cards_to_count):
+        score += math.pow(20, i) * sorted_by_rank[len(cards) - number_of_cards_to_count + i][0]
+
+    return int(score)
 
 class HandEvaluator(object):
     def __init__(self, cards):
@@ -7,26 +21,16 @@ class HandEvaluator(object):
             self.cards = cards
             self.ranks_count, self.suits_count = self.getStatistics(self.cards)
             self.inv_ranks_count = {v: k for k, v in self.ranks_count.items()}
-            self.sorted_flush_cards = sorted(self.getFlushCardsIfExists(), key=lambda x: x[0])
+            self.sorted_flush_cards = sorted(self.getFlushValuesIfFlushExists())
         else:
             raise Exception("no point calculating score without all cards on table")
 
     def getStraightFlushScore(self):
         if len(self.sorted_flush_cards) > 0:
-            longest_streak = [self.sorted_flush_cards[0]]
-
-            for i in range(len(self.sorted_flush_cards) - 1):
-                if self.sorted_flush_cards[i][0] == self.sorted_flush_cards[i + 1][0] - 1:
-                    longest_streak.append(self.sorted_flush_cards[i + 1])
-                else:
-                    if len(longest_streak) >= 5:
-                        break
-                    if len(longest_streak) > len(self.sorted_flush_cards) - i:
-                        break
-                    longest_streak = [self.sorted_flush_cards[i+1]]
+            longest_streak = self.getStraightCardsIfExist(self.sorted_flush_cards)
 
             if len(longest_streak) >= 5:
-                return VALUE_PRESETS["straight_flush"] + self.getHighCardScore(longest_streak)
+                return VALUE_PRESETS["straight_flush"] + self.getXHighestCardScore(longest_streak, 5)
 
         return 0
 
@@ -40,7 +44,7 @@ class HandEvaluator(object):
                     cards_without_four_of_kind.append(card)
 
             return VALUE_PRESETS["four_of_kind"] + \
-                   self.getHighCardScore(cards_without_four_of_kind, number_of_cards_to_count=1)
+                   self.getCardWithXHighestCardScore(cards_without_four_of_kind, number_of_cards_to_count=1)
 
         return 0
 
@@ -53,17 +57,17 @@ class HandEvaluator(object):
                 if rank_appearances == 2 and rank > highest_two_card:
                     highest_two_card = rank
 
-            return VALUE_PRESETS["full_house"] + 10 * rank_appearing_three_times + highest_two_card
+            return VALUE_PRESETS["full_house"] + 20 * rank_appearing_three_times + highest_two_card
 
         return 0
 
-    def getFlushCardsIfExists(self):
+    def getFlushValuesIfFlushExists(self):
         for suit in self.suits_count:
             if self.suits_count[suit] >= 5:
                 flush_cards = []
                 for card in self.cards:
                     if card[1] == suit:
-                        flush_cards.append(card)
+                        flush_cards.append(card[0])
                 return flush_cards
 
         return []
@@ -72,21 +76,86 @@ class HandEvaluator(object):
         if len(self.sorted_flush_cards) >= 5:
             val = 0
             for i in range(5):
-                val += self.sorted_flush_cards[len(self.sorted_flush_cards) - 5 + i][0]
+                val += self.sorted_flush_cards[len(self.sorted_flush_cards) - 5 + i]
             return VALUE_PRESETS["flush"] + val
 
         return 0
 
+    def getStraightCardsIfExist(self, sorted_unique_rank_cards):
+        longest_streak = [sorted_unique_rank_cards[0]]
+
+        for i in range(len(sorted_unique_rank_cards) - 1):
+            if sorted_unique_rank_cards[i] == sorted_unique_rank_cards[i + 1] - 1:
+                longest_streak.append(sorted_unique_rank_cards[i + 1])
+            else:
+                if len(longest_streak) >= 5:
+                    break
+                if len(longest_streak) > len(sorted_unique_rank_cards) - i:
+                    break
+                longest_streak = [sorted_unique_rank_cards[i + 1]]
+
+        return longest_streak
+
     def getStraightScore(self):
+        sorted_unique_ranks = sorted(self.ranks_count.keys())
+        straight_cards = self.getStraightCardsIfExist(sorted_unique_ranks)
+
+        if len(straight_cards) >= 5:
+            return VALUE_PRESETS["straight"] + self.getXHighestCardScore(straight_cards, 5)
+
         return 0
 
+    def removeCardsOfGivenValues(self, cards, values):
+        cards_without_given_values = []
+
+        for card in self.cards:
+            if card[0] not in values:
+                cards_without_given_values.append(card)
+
+        return cards_without_given_values
+
     def getThreeOfKindScore(self):
+        if 3 in self.ranks_count.values():
+            highest_three_card = 0
+            for rank in self.ranks_count:
+                rank_appearances = self.ranks_count[rank]
+                if rank_appearances == 3 and rank > highest_three_card:
+                    highest_three_card = rank
+
+            cards_without_highest_three_card = self.removeCardsOfGivenValues(self.cards, [highest_three_card])
+
+            return VALUE_PRESETS["three_of_kind"] + 10000 * highest_three_card + \
+                   getHighestCardScore(cards_without_highest_three_card, 2)
+
         return 0
 
     def getTwoPairScore(self):
+        number_of_two_pairs = list(self.ranks_count.values()).count(2)
+        if number_of_two_pairs >= 2:
+            two_pair_list = []
+            for rank_count in self.ranks_count:
+                if self.ranks_count[rank_count] == 2:
+                    two_pair_list.append(rank_count)
+
+            sorted_two_pairs = sorted(two_pair_list, reverse=True)
+
+            cards_without_highest_two_pairs = \
+                self.removeCardsOfGivenValues(self.cards,[sorted_two_pairs[0], sorted_two_pairs[1]])
+
+            return VALUE_PRESETS["two_pair"] + 200 * sorted_two_pairs[0] + 10 * sorted_two_pairs[1] + \
+                   self.getCardWithXHighestCardScore(cards_without_highest_two_pairs, 1)
+
         return 0
 
     def getPairScore(self):
+        number_of_pairs = list(self.ranks_count.values()).count(2)
+        if number_of_pairs == 1:
+            pair_value = self.inv_ranks_count[2]
+
+            cards_without_pair = self.removeCardsOfGivenValues(self.cards, [pair_value])
+
+            return VALUE_PRESETS["pair"] + 100000 * pair_value + getHighestCardScore(cards_without_pair, 3)
+
         return 0
 
     def getStatistics(self, cards):
@@ -106,7 +175,11 @@ class HandEvaluator(object):
 
         return ranks,suits
 
-    def getHighCardScore(self, cards, number_of_cards_to_count=5):
+    def getXHighestCardScore(self, values, number_of_cards_to_count):
+        sorted_by_rank = sorted(values)
+        return sorted_by_rank[len(values) - number_of_cards_to_count]
+
+    def getCardWithXHighestCardScore(self, cards, number_of_cards_to_count=5):
         sorted_by_rank = sorted(cards, key=lambda x: x[0])
         return sorted_by_rank[len(cards) - number_of_cards_to_count][0]
 
@@ -119,4 +192,4 @@ class HandEvaluator(object):
             if score != 0:
                 return score
 
-        return self.getHighCardScore(self.cards)
+        return getHighestCardScore(self.cards, 5)
--- pokerMaster.py
+++ pokerMaster.py
@@ -66,8 +66,11 @@ def testStraightFlush():
     assert (getHandScore([[2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 1]]) == straight_flush_preset + 3)
     assert (getHandScore([[2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 1], [8, 1]]) == straight_flush_preset + 2)
     assert (getHandScore([[6, 0], [3, 0], [4, 0], [5, 0], [7, 0], [7, 1], [8, 1]]) == straight_flush_preset + 3)
-    assert (getHandScore([[2, 0], [3, 0], [4, 0], [5, 0], [6, 1], [7, 1], [8, 1]]) == 4)
-    assert (getHandScore([[2, 0], [3, 0], [4, 0], [5, 0], [6, 1], [7, 1], [1, 1]]) == 4)
+    hand = [[2, 0], [3, 0], [4, 0], [5, 0], [11, 1], [7, 1], [8, 1]]
+    assert (getHandScore(hand) == getHighestCardScore(hand, 5))
+    hand = [[6, 0], [3, 0], [4, 0], [5, 0], [11, 1], [9, 1], [1, 1]]
+    hand_with_aces = [[6, 0], [3, 0], [4, 0], [5, 0], [11, 1], [9, 1], [14, 1]]
+    assert (getHandScore(hand) == getHighestCardScore(hand_with_aces, 5))
     assert (getHandScore([[2, 0], [3, 0], [4, 0], [5, 0], [6, 1], [7, 1], [1, 0]]) == straight_flush_preset + 1)
     assert (getHandScore([[12, 0], [11, 0], [10, 0], [13, 0], [6, 1], [7, 1], [1, 0]]) == straight_flush_preset + 10)
 
@@ -78,16 +81,17 @@ def testFourOfAKind():
     assert (getHandScore([[12, 0], [12, 1], [12, 2], [12, 3], [11, 0], [7, 1], [8, 0]]) == four_of_a_kind_preset + 11)
     assert (getHandScore([[12, 0], [12, 1], [12, 2], [12, 3], [1, 0], [7, 1], [8, 0]]) == four_of_a_kind_preset + 14)
     assert (getHandScore([[12, 0], [12, 1], [12, 2], [12, 3], [2, 0], [7, 1], [8, 0]]) == four_of_a_kind_preset + 8)
-    assert (getHandScore([[2, 0], [3, 0], [4, 2], [5, 2], [11, 0], [7, 1], [8, 0]]) == 4)
+    hand = [[2, 0], [3, 0], [4, 2], [5, 2], [11, 0], [7, 1], [8, 0]]
+    assert (getHandScore(hand) == getHighestCardScore(hand, 5))
 
 def testFullHouse():
     full_house_preset = VALUE_PRESETS["full_house"]
 
-    assert (getHandScore([[12, 0], [12, 1], [12, 2], [7, 3], [2, 0], [7, 1], [8, 0]]) == full_house_preset + 120 + 7)
-    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [7, 1], [8, 0]]) == full_house_preset + 20 + 7)
-    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [12, 1], [8, 0]]) == full_house_preset + 20 + 12)
-    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [12, 1], [7, 0]]) == full_house_preset + 20 + 12)
-    assert (getHandScore([[2, 0], [2, 1], [3, 2], [7, 3], [2, 0], [3, 1], [7, 0]]) == full_house_preset + 20 + 7)
+    assert (getHandScore([[12, 0], [12, 1], [12, 2], [7, 3], [2, 0], [7, 1], [8, 0]]) == full_house_preset + 240 + 7)
+    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [7, 1], [8, 0]]) == full_house_preset + 40 + 7)
+    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [12, 1], [8, 0]]) == full_house_preset + 40 + 12)
+    assert (getHandScore([[2, 0], [2, 1], [12, 2], [7, 3], [2, 0], [12, 1], [7, 0]]) == full_house_preset + 40 + 12)
+    assert (getHandScore([[2, 0], [2, 1], [3, 2], [7, 3], [2, 0], [3, 1], [7, 0]]) == full_house_preset + 40 + 7)
 
 def testFlush():
     flush_preset = VALUE_PRESETS["flush"]
@@ -97,13 +101,65 @@ def testFlush():
     assert (getHandScore([[1, 0], [10, 0], [3, 0], [8, 0], [4, 0], [5, 0], [6, 0]]) == flush_preset + 43)
     assert (getHandScore([[1, 0], [10, 0], [3, 0], [8, 0], [4, 0], [5, 0], [7, 0]]) == flush_preset + 44)
 
+def testStraight():
+    straight_preset = VALUE_PRESETS["straight"]
+
+    assert (getHandScore([[12, 0], [12, 1], [11, 0], [10, 3], [9, 0], [8, 1], [7, 3]]) == straight_preset + 8)
+    assert (getHandScore([[13, 0], [1, 1], [11, 0], [10, 3], [9, 0], [8, 1], [7, 3]]) == straight_preset + 7)
+    assert (getHandScore([[2, 0], [1, 1], [3, 0], [4, 3], [5, 0], [8, 1], [7, 3]]) == straight_preset + 1)
+
+def testThreeOfAKind():
+    three_kind_preset = VALUE_PRESETS["three_of_kind"]
+
+    assert (getHandScore([[12, 0], [12, 1], [12, 0], [10, 3], [9, 0], [8, 1], [7, 3]]) ==
+            three_kind_preset + 10000 * 12 + 20 * 10 + 9)
+    assert (getHandScore([[2, 0], [2, 1], [2, 0], [10, 3], [9, 0], [8, 1], [7, 3]]) ==
+            three_kind_preset + 10000 * 2 + 20 * 10 + 9)
+    assert (getHandScore([[6, 0], [6, 1], [6, 0], [1, 3], [13, 0], [8, 1], [7, 3]]) ==
+            three_kind_preset + 10000 * 6 + 20 * 14 + 13)
+
+def testTwoPair():
+    two_pair_preset = VALUE_PRESETS["two_pair"]
+
+    assert (getHandScore([[12, 0], [12, 1], [10, 0], [10, 3], [9, 0], [8, 1], [7, 3]]) == two_pair_preset + 2400 + 100 + 9)
+    assert (getHandScore([[12, 0], [12, 1], [10, 0], [10, 3], [8, 0], [8, 1], [7, 3]]) == two_pair_preset + 2400 + 100 + 8)
+    assert (getHandScore([[12, 0], [12, 1], [10, 0], [10, 3], [1, 0], [1, 1], [7, 3]]) == two_pair_preset + 2800 + 120 + 10)
+    assert (getHandScore([[2, 0], [2, 1], [3, 0], [6, 3], [1, 0], [1, 1], [4, 3]]) == two_pair_preset + 2800 + 20 + 6)
+    assert (getHandScore([[12, 0], [12, 1], [8, 0], [1, 3], [13, 0], [13, 1], [7, 3]]) == two_pair_preset + 2600 + 120 + 14)
+
+def testPair():
+    pair_preset = VALUE_PRESETS["pair"]
+
+    assert (getHandScore([[13, 0], [13, 1], [10, 0], [5, 3], [9, 0], [8, 1], [7, 3]]) ==
+            pair_preset + 100000 * 13 + 400 * 10 + 20 * 9 + 8)
+    assert (getHandScore([[12, 0], [12, 1], [10, 0], [5, 3], [9, 0], [8, 1], [7, 3]]) ==
+            pair_preset + 100000 * 12 + 400 * 10 + 20 * 9 + 8)
+    assert (getHandScore([[1, 0], [1, 1], [10, 0], [5, 3], [9, 0], [4, 1], [7, 3]]) ==
+            pair_preset + 100000 * 14 + 400 * 10 + 20 * 9 + 7)
+
+def testHighCard():
+    assert (getHandScore([[13, 0], [1, 1], [10, 0], [5, 3], [9, 0], [8, 1], [7, 3]]) ==
+            160000 * 14 + 8000 * 13 + 400 * 10 + 20 * 9 + 8)
+    assert (getHandScore([[13, 0], [1, 1], [10, 0], [11, 3], [9, 0], [8, 1], [6, 3]]) ==
+            160000 * 14 + 8000 * 13 + 400 * 11 + 20 * 10 + 9)
+    assert (getHandScore([[13, 0], [1, 1], [12, 0], [11, 3], [2, 0], [3, 1], [4, 3]]) ==
+            160000 * 14 + 8000 * 13 + 400 * 12 + 20 * 11 + 4)
+    assert (getHandScore([[13, 0], [1, 1], [12, 0], [6, 3], [2, 0], [3, 1], [4, 3]]) ==
+            160000 * 14 + 8000 * 13 + 400 * 12 + 20 * 6 + 4)
+    assert (getHandScore([[13, 0], [1, 1], [11, 0], [10, 3], [2, 0], [3, 1], [9, 3]]) ==
+            160000 * 14 + 8000 * 13 + 400 * 11 + 20 * 10 + 9)
+
 def testHands():
     testStraightFlush()
     testFourOfAKind()
     testFullHouse()
     testFlush()
+    testStraight()
+    testThreeOfAKind()
+    testTwoPair()
+    testPair()
+    testHighCard()
 
 if __name__ == '__main__':
     initial_table = Table()
     testHands()
-    #print(getHandScore([[2, 0], [3, 1], [4, 2], [5, 2], [2, 4], [7, 3], [9, 1]]))
