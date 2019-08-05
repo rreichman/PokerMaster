7ea943b7099748fa501c136841ed853956953946
ranreichman <ran.reichman@snip.today>
2019-08-04 20:35:29 -0700

    added gameplay. about to add division of winnings

--- playerModel.py
+++ playerModel.py
@@ -2,13 +2,16 @@
 ML model of the poker player
 '''
 
+import random
 import tensorflow as tf
 
 class Action(object):
     # Type is Raise;Call;Fold and value is how much (only relevant for Raises)
-    def __init__(self, type, value):
+    def __init__(self, type, value = 0):
         self.type = type
         self.value = value
+
+actions = [Action("Raise", 100), Action("Check"), Action("Fold")]
 
 class PlayerModel(object):
     def __init__(self):
@@ -17,4 +20,7 @@ class PlayerModel(object):
     # NOTE - Currently we keep other player's cards in the state. Make sure that you're not looking at other player's
     # cards (using them in the model)!
     def act(self, state):
-        return Action("Raise", 100)
+        # Start with a random policy that just goes all-in, calls, or folds arbitrarily. Once the workflow is good for
+        # that we can move to more interesting policies.
+        action_index = random.randint(0,len(actions) - 1)
+        return actions[action_index]
--- pokerMaster.py
+++ pokerMaster.py
@@ -9,23 +9,128 @@ from datetime import datetime
 
 import random
 
+INITIAL_STACK_SIZE = 1500
+NUM_OF_PLAYERS = 6
+
 class Table(object):
     def __init__(self):
         self.deck = Deck()
 
-        INITIAL_STACK_SIZE = 1500
-
-        self.player_stacks = [INITIAL_STACK_SIZE] * 6
-        self.player_bets = [0] * 6
+        self.player_stacks = [INITIAL_STACK_SIZE] * NUM_OF_PLAYERS
+        self.player_bets = [0] * NUM_OF_PLAYERS
+        self.players_in_round = [True] * NUM_OF_PLAYERS
+        self.number_of_players_in_round = NUM_OF_PLAYERS
         self.open_cards = []
         # Assuming here that ego is always in position 0
         self.dealer_position = random.randint(0, 5)
         self.player_cards = self.deck.dealCards(number_of_players=6)
 
+        self.big_blind_size = INITIAL_STACK_SIZE / 50
+        self.player_bets[self.dealer_position] = self.big_blind_size / 2
+        self.player_bets[(self.dealer_position + 1) % NUM_OF_PLAYERS] = self.big_blind_size
+        self.last_raiser = (self.dealer_position + 1) % NUM_OF_PLAYERS
+
+        self.highest_bet = self.big_blind_size
+        self.pot_size = self.big_blind_size * 3 / 2
+
+        # TODO:: in the future maybe handle cases where players leave the table
+        self.current_player_index = (self.dealer_position + 2) % NUM_OF_PLAYERS
+
+    def runGame(self):
+        self.print()
+        self.runRound()
+        if self.number_of_players_in_round > 1:
+            for i in range(3):
+                self.open_cards.append(self.deck.pop())
+            self.runRound()
+            if self.number_of_players_in_round > 1:
+                self.open_cards.append(self.deck.pop())
+                self.runRound()
+                if self.number_of_players_in_round > 1:
+                    self.open_cards.append(self.deck.pop())
+                    self.runRound()
+
+    def runRound(self):
+        self.endRound = False
+        while not self.endRound:
+            table.getAction(player_model.act(table))
+            table.print()
+
+        if len(self.open_cards) == 5:
+            self.divideSpoils()
+
+    def divideSpoils(self):
+        # TODO:: this is a bit complex, make sure you do it right.
+        winners = self.getWinners()
+        for i in range(len(self.player_bets)):
+            self.player_stacks[i] -= self.player_bets[i]
+
+        prize_per_winner = self.pot_size / len(winners)
+
+    def getAction(self, action):
+        action_string = "\nPlayer " + str(self.current_player_index) + " played " + action.type
+
+        if action.type == "Raise":
+            # TODO:: maybe make sure that the bet is legal
+            action_string += ", value: " + str(action.value)
+            self.player_bets[self.current_player_index] = self.highest_bet + action.value
+            self.highest_bet = self.player_bets[self.current_player_index]
+            self.last_raiser = self.current_player_index
+        else:
+            if self.last_raiser == self.getNextPlayerIndex():
+                self.endRound = True
+            if action.type == "Check":
+                self.player_bets[self.current_player_index] = self.highest_bet
+            elif action.type == "Fold":
+                self.players_in_round[self.current_player_index] = False
+                self.number_of_players_in_round -= 1
+
+        self.current_player_index = self.getNextPlayerIndex()
+
+        self.pot_size = 0
+        for i in range(len(self.player_bets)):
+            self.pot_size += self.player_bets[i]
+
+        print(action_string)
+
+    def getNextPlayerIndex(self):
+        next_player_index = self.current_player_index
+        while True:
+            next_player_index = (next_player_index + 1) % NUM_OF_PLAYERS
+            if self.players_in_round[next_player_index]:
+                return next_player_index
+
+    def moveRaise(self, player_index, value):
+        self.player_bets[player_index] += value
+
+    def print(self):
+        print("Dealer position: " + str(self.dealer_position))
+        print("Player bets:")
+        print(self.player_bets)
+        print("Players in round:")
+        print(self.players_in_round)
+        print("Pot size: " + str(self.pot_size))
+        print("\nOpen cards:")
+        open_cards_string = ""
+        if len(self.open_cards) == 0:
+            open_cards_string = "[]"
+        for i in range(len(self.open_cards)):
+            open_cards_string += self.open_cards[i].getString() + ","
+        print(open_cards_string)
+        #for i in range(len(self.player_cards))
+        print("Player cards:")
+        for i in range(len(self.player_cards)):
+            cards = self.player_cards[i]
+            print("Player " + str(i) + ". Cards: " + cards[0].getString() + ", " + cards[1].getString())
+        print("Last raiser: " + str(self.last_raiser))
+
 class Card(object):
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+
+    def getString(self):
+        return "Rank: " + str(self.rank) + ", Suit: " + str(self.suit)
 
 class Deck(object):
     def __init__(self):
@@ -48,12 +153,13 @@ class Deck(object):
         return player_cards
 
 if __name__ == '__main__':
-    initial_table = Table()
+    table = Table()
     player_model = PlayerModel()
-    player_model.act(initial_table)
+    table.print()
+    table.runGame()
 
     # Currently allows about 13,000 hand scorings per second. If turns out to be a bottleneck will improve later.
-    print(datetime.now())
+    '''print(datetime.now())
     for i in range(50000):
         testHands()
-    print(datetime.now())
+    print(datetime.now())'''
